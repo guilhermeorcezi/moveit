449e28876820ab813c4484da62f3a860113e1691
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2021-02-23 14:02:50 -0300

    fix: SEO component

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -2,20 +2,16 @@ import Head from 'next/head';
 
 interface SEOProps {
     title: string;
-    description: string;
-    image: string;
     isIndex?: boolean;
 }
 
-export default function SEO({ title, description, image, isIndex }: SEOProps) {
-    const pageImage = image ? `${process.env.NEXT_PUBLIC_SITE_URL}${image}` : null;
-    const pageTitle = isIndex ? title : `${title} | Camilly Matsushima`;
+export default function SEO({ title, isIndex }: SEOProps) {
+    const pageTitle = isIndex ? title : `${title} | MoveIt`;
 
     return (
         <Head>
             <title>{pageTitle}</title>
 
-            <meta name="description" content={description} />
             <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
             <meta name="MobileOptimized" content="320" />
             <meta name="HandheldFriendly" content="True" />
