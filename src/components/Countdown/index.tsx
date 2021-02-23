c8b252e1e857a92d1144886f6c687de77193c41e
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2021-02-23 14:24:49 -0300

    feat: countdown working

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,25 +1,47 @@
+import { useState, useCallback, useEffect } from 'react';
 import SEO from '../SEO';
 import {Container, TimeGroup, TimeSeparator, CountdownButton} from './styles';
 
 export default function Countdown(){
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2,'0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2,'0').split('');
+
+  const startCountdown = useCallback(() => {
+    setActive(true);
+  }, []);
+
+  useEffect(() => {
+    if(active && time > 0){
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000)
+    }
+  }, [active, time])
+
   return(
     <>
     <Container>
       <SEO title="Início | MoveIt" isIndex />
       <TimeGroup>
-        <span>2</span>
-        <span>5</span>
+        <span>{minuteLeft}</span>
+        <span>{minuteRight}</span>
       </TimeGroup>
 
       <TimeSeparator>:</TimeSeparator>
 
       <TimeGroup>
-        <span>0</span>
-        <span>0</span>
+        <span>{secondLeft}</span>
+        <span>{secondRight}</span>
       </TimeGroup>
     </Container>
 
-    <CountdownButton type="button">Iniciar um ciclo</CountdownButton>
+    <CountdownButton type="button" onClick={startCountdown}>Iniciar um ciclo</CountdownButton>
     </>
   )
 }
